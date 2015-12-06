fd48b7818df3632b77fd473e61b5de7150ae2de1
Ignasi Vegas <ignasivegas@gmail.com>
2015-12-06 18:09:20 -0500

    Implemented Swipeable-Cards library

--- app/src/main/java/com/mc/jaspfood/MainActivity.java
+++ app/src/main/java/com/mc/jaspfood/MainActivity.java
@@ -1,15 +1,25 @@
 package com.mc.jaspfood;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+
+import com.andtinder.model.CardModel;
+import com.andtinder.model.Orientations;
+import com.andtinder.view.CardContainer;
+import com.andtinder.view.SimpleCardStackAdapter;
+
 public class MainActivity extends AppCompatActivity {
+
+    private CardContainer mCardContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +36,33 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        Resources r = getResources();
+
+        mCardContainer = (CardContainer) findViewById(R.id.layoutview);
+
+        mCardContainer.setOrientation(Orientations.Orientation.Disordered);
+
+        SimpleCardStackAdapter adapter = new SimpleCardStackAdapter(this);
+
+        //CardModel card = new CardModel("Title1", "Description goes here", r.getDrawable(R.drawable.picture1);
+
+        adapter.add(new CardModel("Title1", "Description goes here", r.getDrawable(R.drawable.picture1)));
+
+        CardModel cardModel = new CardModel("Title1", "Description goes here", r.getDrawable(R.drawable.picture1));
+        cardModel.setOnClickListener(new CardModel.OnClickListener() {
+            @Override
+            public void OnClickListener() {
+                Log.i("Swipeable Cards","I am pressing the card");
+            }
+        });
+
+
+        adapter.add(cardModel);
+
+        mCardContainer.setAdapter(adapter);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
